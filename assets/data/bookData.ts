8941d590a50ee5fbcc0c845a94b9bc3d7720c399
serg-sheridan <52678820+serg-sheridan@users.noreply.github.com>
2019-08-15 11:12:46 -0400

    Add files via upload

---
+++ assets/data/bookData.ts
@@ -0,0 +1,7 @@
+import { Book } from '../../app/book'
+
+export const MYBOOKS : Book[] = [
+    { author : "John Does", country : "US", category : "Fantasy"},
+    { author : "Amy Tower", country : "UK", category : "Romance"},
+    { author : "Jack Redd", country : "US", category : "Biography"}
+];
